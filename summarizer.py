babc01a67b058a4dfaa0156f05f1b055a5aba102
RAGHAVENDRA NATH CHATURVEDI <rnc93056@gmail.com>
2026-03-08 19:45:49 +0530

    Refactor summarization to use tokenizer and model

--- summarizer.py
+++ summarizer.py
@@ -1,25 +1,38 @@
 import streamlit as st
-from transformers import pipeline
+from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
 
 @st.cache_resource
 def load_model():
-    summarizer = pipeline(
-        task="summarization",
-        model="sshleifer/distilbart-cnn-12-6"
-    )
-    return summarizer
 
-summarizer = load_model()
+    model_name = "sshleifer/distilbart-cnn-12-6"
+
+    tokenizer = AutoTokenizer.from_pretrained(model_name)
+    model = AutoModelForSeq2SeqLM.from_pretrained(model_name)
+
+    return tokenizer, model
+
+
+tokenizer, model = load_model()
+
 
 def summarize_text(text):
 
     text = text[:2000]
 
-    result = summarizer(
+    inputs = tokenizer(
         text,
-        max_length=120,
-        min_length=30,
-        do_sample=False
+        return_tensors="pt",
+        max_length=1024,
+        truncation=True
     )
 
-    return result[0]["summary_text"]
+    summary_ids = model.generate(
+        inputs["input_ids"],
+        max_length=120,
+        min_length=30,
+        num_beams=4
+    )
+
+    summary = tokenizer.decode(summary_ids[0], skip_special_tokens=True)
+
+    return summary
